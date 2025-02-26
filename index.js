562bc3e69f0d2952daff0cf343d9dc45c1e606c0
chien-pv <phamvanbk11@gmail.com>
2025-02-26 14:54:09 +0700

    setting static

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(morgan("combined"));
 app.set("views", "./views");
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
