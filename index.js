e4a343fb516f261ba3cd22cde162c9e441e528a1
chien-pv <phamvanbk11@gmail.com>
2025-02-26 14:11:35 +0700

    demo

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+var morgan = require("morgan");
+
+const app = express();
+app.use(morgan("combined"));
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, () => {
+  console.log("Server started!!!");
+});
